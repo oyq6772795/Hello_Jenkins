d7b954f39d4c04eeeda4148bd217642b87371e92
欧阳钦 <498828493@qq.com>
2018-09-18 16:21:30 +0800

    test

---
+++ Hello_Jenkins/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hello_Jenkins
+//
+//  Created by 欧阳钦 on 2018/9/18.
+//  Copyright © 2018年 欧阳钦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Hello_Jenkins/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  Hello_Jenkins
+//
+//  Created by 欧阳钦 on 2018/9/18.
+//  Copyright © 2018年 欧阳钦. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    //更改背景颜色
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
